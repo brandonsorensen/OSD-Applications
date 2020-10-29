5b6dda4e1e747f26653499f3a1d4512cb5bcd79e
Brandon Sorensen <brandonsorensen@protonmail.com>
2020-10-29 15:28:59 +0100

    Compatibility with Python 2

--- grade-reports/merge_files.py
+++ grade-reports/merge_files.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # coding: utf-8
+from builtins import input
 from datetime import datetime
 from numbers import Number
-from os import getcwd, PathLike
-from pathlib import Path
-from typing import Optional
+from os import getcwd, path
 import argparse
 import logging
-import math
 import platform
 
 import numpy as np
@@ -25,19 +23,20 @@ class GradeConverter(object):
 
     grade_cats = list(reversed([letter for grade, letter in cutoffs] + ['F']))
 
-    def __call__(self, grade: Number) -> str:
+    def __call__(self, grade):
+        # type: (Number) -> str
         if grade < 59.5:
             return 'F'
 
-        high = math.inf
+        high = float('inf')
         for low, letter in self.cutoffs:
             if low <= grade < high:
                 return letter
             high = low
 
 
-def to_categories(df: pd.DataFrame, thresh: float = 0.25,
-                  inplace: bool = False) -> Optional[pd.DataFrame]:
+def to_categories(df, thresh=0.25, inplace=False):
+    # type: (pd.DataFrame, float, bool) -> pd.DataFrame
     """
     Converts all the string columns in a pandas DataFrame to
     categories if the number of unique elements in that
@@ -58,7 +57,8 @@ def to_categories(df: pd.DataFrame, thresh: float = 0.25,
         return df
 
 
-def make_byu(path: PathLike) -> pd.DataFrame:
+def make_byu(path):
+    # type: (str) -> pd.DataFrame
 
     byu_to_keep = ['percentage', 'letter grade', 'Student Name',
                    'Course', 'Teacher Name']
@@ -79,8 +79,8 @@ def make_byu(path: PathLike) -> pd.DataFrame:
     return byu
 
 
-def make_apex(path: PathLike, drop_null_course_id: bool = True,
-              filter_future: bool = True) -> pd.DataFrame:
+def make_apex(path, drop_null_course_id=True, filter_future=True):
+    # type: (str, bool, bool) -> pd.DataFrame
 
     apex_to_keep = [
         'ImportClassroomID', 'ClassroomName', 'TeacherName',
@@ -126,12 +126,13 @@ def make_apex(path: PathLike, drop_null_course_id: bool = True,
     apex = apex[~apex['student']
                 .str.lower()
                 .str.contains('demo')].reset_index(drop=True)
-    apex.drop_duplicates(ignore_index=True, inplace=True)
+    apex = apex.drop_duplicates().reset_index(drop=True)
 
     return to_categories(apex)
 
 
-def make_idla(path: PathLike, filter_future: bool = True) -> pd.DataFrame:
+def make_idla(path, filter_future=True):
+    # type (str, bool) -> pd.DataFrame
     idla_to_keep = ['Student', 'Course', 'Start Date', 'End Date',
                     'Teacher', 'TeacherEmail', 'Grade']
     idla_cols = ['student', 'course', 'start_date', 'end_date',
@@ -155,7 +156,8 @@ def make_idla(path: PathLike, filter_future: bool = True) -> pd.DataFrame:
     return to_categories(idla)
 
 
-def make_schoology(path: PathLike) -> pd.DataFrame:
+def make_schoology(path):
+    # type (str) -> pd.DataFrame
     schoology_cols = ['student_first', 'student_last', 'student_email',
                       'course', 'course_code', 'section', 'titles',
                       'grade', 'letter_grade']
@@ -167,13 +169,14 @@ def make_schoology(path: PathLike) -> pd.DataFrame:
     school['section'] = (school['section']
                          .str.replace('Section ', '')
                          .astype('uint8'))
-    school.drop_duplicates(ignore_index=True, inplace=True)
+    school = school.drop_duplicates().reset_index(drop=True)
 
     school['student'] = school['student_last'] + ', ' + school['student_first']
     return to_categories(school)
 
 
-def make_student_list(path: PathLike) -> pd.DataFrame:
+def make_student_list(path):
+    # type (str) -> pd.DataFrame
     students = pd.read_csv('student-schools.csv')
     students.columns = (students.columns.str.replace('STUDENTS.', '')
                         .str.lower())
@@ -186,10 +189,13 @@ def make_student_list(path: PathLike) -> pd.DataFrame:
     return students
 
 
-def merge_sources(byu: pd.DataFrame, apex: pd.DataFrame,
-                  idla: pd.DataFrame, school: pd.DataFrame,
-                  students: pd.DataFrame) -> pd.DataFrame:
-
+def merge_sources(byu,      # type: pd.DataFrame
+                  apex,     # type: pd.DataFrame
+                  idla,     # type: pd.DataFrame
+                  school,   # type: pd.DataFrame
+                  students  # type: pd.DataFrame
+                  ):
+    # type: (...) -> pd.DataFrame
     source_map = {
         'byu': byu,
         'apex': apex,
@@ -204,13 +210,14 @@ def merge_sources(byu: pd.DataFrame, apex: pd.DataFrame,
 
     out = pd.concat([
         df.assign(source=source) for source, df in source_map.items()
-    ], ignore_index=True, join='inner')
+    ], join='inner').reset_index(drop=True)
 
     out = (out.merge(students['last_first'].reset_index(),
                      left_on='student',
                      right_on='last_first', how='left')
            .drop(columns='last_first')
-           .drop_duplicates(ignore_index=True))
+           .drop_duplicates()
+           .reset_index(drop=True))
     out['student_number'] = out['student_number'].astype('UInt64')
     out['letter_grade'] = pd.Categorical(out['grade'].apply(GradeConverter()),
                                          categories=GradeConverter.grade_cats,
@@ -220,36 +227,38 @@ def merge_sources(byu: pd.DataFrame, apex: pd.DataFrame,
             .reset_index(drop=True))
 
 
-def to_csv(df: pd.DataFrame, path: PathLike):
+def to_csv(df, path):
+    # type: (pd.DataFrame, str)
     (df.sort_values(by=['student_last', 'student_first', 'source'])
      .reset_index(drop=True)
      .to_csv(path, index=False))
 
 
-def parse_args() -> argparse.Namespace:
-    default = Path('reports')
+def parse_args():
+    # type: () -> argparse.Namespace
+    default = 'reports'
     formatter = argparse.ArgumentDefaultsHelpFormatter
 
     parser = argparse.ArgumentParser(description='Merge grade reports into '
                                                  'one file.',
                                      formatter_class=formatter)
     parser.add_argument('-b', '--byu', type=str, nargs=1,
-                        default=default/'byu.xlsx',
+                        default=path.join(default, 'byu.xlsx'),
                         help='path to the BYU file')
     parser.add_argument('-a', '--apex', type=str, nargs=1,
-                        default=default/'apex.csv',
+                        default=path.join(default, 'apex.csv'),
                         help='path to the Apex file')
     parser.add_argument('-i', '--idla', type=str, nargs=1,
-                        default=default/'idla.csv',
+                        default=path.join(default, 'idla.csv'),
                         help='path to the IDLA file')
     parser.add_argument('-s', '--schoology', type=str, nargs=1,
-                        default=default/'schoology.csv',
+                        default=path.join(default, 'schoology.csv'),
                         help='path to the Schoology file')
     parser.add_argument('-p', '--ps-students', type=str, nargs=1,
-                        default=default/'student-schools.csv',
+                        default=path.join(default, 'student-schools.csv'),
                         help='path to the PowerSchool students file')
     parser.add_argument('-o', '--output-path', type=str, nargs=1,
-                        default=Path(getcwd()),
+                        default=getcwd(),
                         help='path to the PowerSchool students file')
     parser.add_argument('-f', '--keep-future', action='store_true',
                         help='exclude classes that start in the future')
@@ -262,24 +271,25 @@ def parse_args() -> argparse.Namespace:
 def main():
     args = parse_args()
 
-    logging.basicConfig(level=logging.ERROR if args.silence_output
-                        else logging.INFO)
+    level = logging.ERROR if args.silence_output else logging.INFO
+    logging.basicConfig(level=level, format='%(message)s')
 
     logger = logging.getLogger(__name__)
-    out_path = Path(args.output_path)
-    if out_path.is_dir():
-        out_path /= 'grade-reports.csv'
+    out_path = args.output_path
+    if path.isdir(out_path):
+        out_path = path.join(out_path, 'grade-reports.csv')
 
     byu = make_byu(args.byu)
-    logger.info(f'Found BYU file at "{args.byu}".')
+    logger.info('Found BYU file at "{}".'.format(args.byu))
     apex = make_apex(args.apex, filter_future=not args.keep_future)
-    logger.info(f'Found Apex file at "{args.apex}".')
+    logger.info('Found Apex file at "{}".'.format(args.apex))
     idla = make_idla(args.idla, filter_future=not args.keep_future)
-    logger.info(f'Found IDLA file at "{args.idla}".')
+    logger.info('Found IDLA file at "{}".'.format(args.idla))
     school = make_schoology(args.schoology)
-    logger.info(f'Found Schoology file at "{args.schoology}".')
+    logger.info('Found Schoology file at "{}".'.format(args.schoology))
     students = make_student_list(args.ps_students)
-    logger.info(f'Found PowerSchool student file at "{args.ps_students}".')
+    logger.info('Found PowerSchool student file at "{}".'
+                .format(args.ps_students))
 
     logger.info('Merging and standardizing files.')
     out = merge_sources(byu, apex, idla, school, students)
@@ -287,15 +297,19 @@ def main():
     unknowns = out[out['student_number'].isnull()]
     n_unknown = len(unknowns)
     if n_unknown and not args.silence_output:
-        unknown_path = out_path.parent/'unknown-students.csv'
-        unknown_path = unknown_path.relative_to(getcwd())
-        logger.info(f'{n_unknown} student were not found in student file.'
-                    f' Saving those entries to "{unknown_path}".')
+        unknown_path = path.join(out_path, 'unknown-students.csv')
+        unknown_path = path.dirname(unknown_path)
+        unknown_path = path.relpath(unknown_path)
+        logger.info('{} student were not found in student file.'
+                    .format(n_unknown)
+                    + ' Saving those entries to "{}".'
+                    .format(unknown_path))
         (unknowns.drop(columns='student_number')
          .to_csv(unknown_path, index=False))
 
     out.to_csv(out_path, index=False)
-    logger.info(f'Output file saved to "{out_path.relative_to(getcwd())}".')
+    logger.info('Output file saved to "{}".'
+                .format(path.relpath(out_path)))
     logger.info('Printing 10 random rows from output as an example:')
     if not args.silence_output:
         print(out.sample(10).to_string(index=False))
